98ae08b42b73726e88b7531cd393bc9eda3a1ab1
Kali Correll <kalicorrell18@gmail.com>
2020-11-11 20:47:59 -0500

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { HelpComponent } from "./help/help.component";
+import { WorkComponent } from "./work/work.component";
+import { EducationComponent } from "./education/education.component";
+import { E404Component } from "./e404/e404.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "help", component: HelpComponent },
+  { path: "work", component: WorkComponent },
+  { path: "education", component: EducationComponent },
+  { path: "**", component: E404Component }
+ 
+  
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
